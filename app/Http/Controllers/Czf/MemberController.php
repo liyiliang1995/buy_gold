590d422ee5e4d083efe3a19a5b741c68a6341c00
yxx <1365831278@qq.com>
2019-07-24 13:56:38 +0800

    创建用户

--- app/Http/Controllers/Czf/MemberController.php
+++ app/Http/Controllers/Czf/MemberController.php
@@ -14,6 +14,7 @@ class MemberController extends Controller
 
     public function getUserSet()
     {
+        dd(config('czf.test'));
         return view('czf.userset');
     }
 }
---
+++ config/czf.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: youxingxiang
+ * Date: 2019/7/24
+ * Time: 1:40 PM
+ */
+return [
+    'test' => '这是一个测试',
+];
